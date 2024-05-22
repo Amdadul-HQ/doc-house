344f45b7c99143dbf76f7604c489716fae6def90
AmdadulHQ <rimonamdadul301@gmail.com>
2024-05-22 16:22:04 +0600

    adding first comment

---
+++ doc-house-client/src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<MainLayout/>,
+        errorElement:<ErrorPage/>,
+        children:[
+            {
+                path:'/',
+                element:<Home/>
+            }
+        ]
+    }
+])
